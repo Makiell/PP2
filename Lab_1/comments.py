69c33c399b76a68744c3a586526d7ad62adfb77f
Damir <damir.sharipov99@mail.ru>
2023-01-26 13:50:05 +0600

    comments

---
+++ Lab_1/comments.py
@@ -0,0 +1,8 @@
+# Python Comments 
+# #This is a comment
+
+"""
+This is a comment
+written in 
+more than just one line
+"""
--- Lab_1/syntax.py
+++ Lab_1/syntax.py
@@ -5,13 +5,4 @@
 # if 5 > 2:
 # print("Five is greater than two!")
 
-# Python Comments 
-# #This is a comment
-
-"""
-This is a comment
-written in 
-more than just one line
-"""
-
 syn
