4ba46c6e615839ffc843fc749fb201962e2538f7
Damir <damir.sharipov99@mail.ru>
2023-01-26 13:37:21 +0600

    Variables

---
+++ Lab_1/syntax.py
@@ -0,0 +1,15 @@
+# Python Syntax
+
+# print("Hello World")
+
+# if 5 > 2:
+# print("Five is greater than two!")
+
+# Python Comments 
+# #This is a comment
+
+"""
+This is a comment
+written in 
+more than just one line
+"""
