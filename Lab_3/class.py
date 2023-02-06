737834110252dc9300d46f05dd1abe6e4b6f1b10
Damir <damir.sharipov99@mail.ru>
2023-02-06 19:46:13 +0600

    class lab 3

--- Lab_3/class.py
+++ Lab_3/class.py
@@ -102,21 +102,21 @@
 
 # 6
 
-def filter(list):
-    result = []
-    for i in list:
-        count=0
-        for j in range(1, i+1):
-            if(i%j==0):
-                count += 1
+# def filter(list):
+#     result = []
+#     for i in list:
+#         count=0
+#         for j in range(1, i+1):
+#             if(i%j==0):
+#                 count += 1
 
-        if(count==2):
-            result.append(i)
+#         if(count==2):
+#             result.append(i)
 
-    return result
+#     return result
                 
-a=[1,2,4,7,8]
+# a=[1,2,4,7,8]
 
-print(filter(a))
+# print(filter(a))
 
 
