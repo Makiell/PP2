bd0bd45bcd13ec2751132a53458161374080d48f
Damir <damir.sharipov99@mail.ru>
2023-02-16 12:15:12 +0600

    date

---
+++ Lab_4/date.py
@@ -0,0 +1,33 @@
+import datetime
+# 1
+
+
+# x = datetime.datetime.today()
+
+# dt = x - datetime.timedelta(5)
+
+# print(dt)
+
+# 2
+
+# x = datetime.date.today()
+
+# delta = datetime.timedelta(1)
+
+# print(x-delta)
+# print(x)
+# print(x+delta)
+
+# 3
+
+# x=datetime.datetime.today()
+
+# print(x.strftime("%Y" + " " + "%b" + " " + "%d" + "  %H" + ":%M:%S"))
+
+
+# 4
+
+# x = datetime.datetime.today()
+# epoch = datetime.datetime(1970, 1, 1)
+
+# print((x-epoch).total_seconds())
