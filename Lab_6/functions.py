7f74ecf9218246e180e7d85ff108e37045d2f065
Damir <damir.sharipov99@mail.ru>
2023-02-20 20:18:39 +0600

    functions

---
+++ Lab_6/functions.py
@@ -0,0 +1,75 @@
+# 1
+
+# def mult(list):
+#     result = 1
+#     for i in list:
+#         result = result * i
+    
+#     return result
+
+# list = [int(input()) for i in range(int(input()))]
+
+# print(mult(list))
+
+
+
+# 2
+
+# str = str(input())
+
+# count=0
+# for i in str:
+#     if i.isupper():
+#         count+=1
+
+# print(count)
+
+
+
+# 3
+
+# str = str(input())
+
+
+# str2=reversed(str)
+# if list(str)==list(str2):
+#     print("Palindrome")
+# else:
+#     print('Not palindrome')
+
+
+
+# str2 = str[::-1]
+# if (str)==(str2):
+#     print("Palindrome")
+# else:
+#     print('Not palindrome')
+
+
+
+# 4
+
+# from time import sleep  ???
+# import math
+
+# number = int(input())
+
+# milliseconds = int(input())
+
+# sleep(milliseconds/1000)
+
+# print("Square root of {n} after {m} miliseconds is {sq}".format(n=number, m= milliseconds, sq = math.sqrt(number)))
+
+
+
+# 5
+
+# def isTrue(tuple):
+#     for i in tuple:
+#         if not i:
+#             return False
+#     return True
+
+# tuple = (True, True)
+
+# print(isTrue(tuple))
