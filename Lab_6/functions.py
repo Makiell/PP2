f715121e3dcc9237e0078629e96a20ecfb5dbea4
Damir <damir.sharipov99@mail.ru>
2023-03-09 11:46:21 +0600

    func

--- Lab_6/functions.py
+++ Lab_6/functions.py
@@ -10,17 +10,17 @@
 
 # 2
 
-str = str(input())
+# str = str(input())
 
-lower=0
-upper=0
-for i in range(len(str)):
-    if 65 <= ord(str[i]) <= 90:
-        lower+=1
-    else:
-        upper+=1
+# lower=0
+# upper=0
+# for i in range(len(str)):
+    # if 65 <= ord(str[i]) <= 90:
+        # lower+=1
+    # else:
+        # upper+=1
 
-print(lower, upper)
+# print(lower, upper)
 
 
 
